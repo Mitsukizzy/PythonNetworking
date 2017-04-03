0d68be798073052265bdc7a77935ce67e1e0d3d5
Izzy Benavente <ibenaven@usc.edu>
2017-04-02 21:41:26 -0700

    Setup for ping tool

--- NetworkTraffic/ping.py
+++ NetworkTraffic/ping.py
@@ -1,106 +1,114 @@
-""" Assignment 1 - CSCI 353 Spring 2017
+""" Assignment 2 - CSCI 353 Spring 2017
     Isabella Benavente
     Using Python 2.7.12 
-    CLIENT Module 
-    USAGE: $ python client.py -s 127.0.0.1 -p 7022 -l client1log.txt -n Alice
-    USAGE: $ python client.py -s 127.0.0.1 -p 7021 -l client2log.txt -n Bob
+    Ping Tool Module 
+    USAGE: $ python ping.py -d 206.190.36.45 -c 4 -p "hello"
 """
 #!/usr/bin/python2 
-# Clients communicate using UDP
 
 import socket
 import sys, time
+import struct
 import getopt
 import logging
 import threading
 
-class Client():
+ECHO_REQUEST = 8
+
+class Ping():
     
-    serverip = ''
-    port = ''
-    logfile = ''
-    clientname = ''
+    payload = ''
+    count = ''
+    dst = ''
+
+    sent = 0
+    rcvd = 0
+    lost = 0
+    TTL = 64
     
     def __init__(self):        
-        # Init socket, parameters are to specify internet and UDP
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # Init socket, parameters are to specify internet, raw socket, and int type
+        # Need to run as root for ICMP (use sudo)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
         self.shouldExit = False
 
-    def send(self):    
-        message = ''
-        while self.shouldExit == False:
-            message = raw_input("")
+    def send(self, dst, payload):    
+        idnum = self.sent
+        packet = struct.pack("bbHHh", ECHO_REQUEST, 0, idnum, idnum, 1)
+        packet += payload
+        print "Pinging " + dst + " with " + str(len(payload)) + " bytes of data \"" + payload + "\""
 
-            if message == "exit":
-                self.shouldExit = True
-                break
-
-            msgparts = message.split(' ')
-            # Expected message input is "sendto <client> <message>"
-            if msgparts[0] != 'sendto' or len(msgparts) <= 2:
-                print "Message format incorrect, part0 is " + msgparts[0] + " and len is " + str( len(msgparts) )
-                continue
-            
-            logging.info(message)
-
-            # Construct message in the format "sendto <client> message <message>"
-            index = message.find(msgparts[2])
-            message = message[:index] + 'message ' + message[index:]
-            self.sock.sendto(message, (self.serverip, self.port))
+        self.sock.sendto(packet, (self.src, self.port))
+        self.sent += 1
 
     def receive(self):
-        print "waiting for messages.."
         while self.shouldExit == False:
             data = self.sock.recvfrom(1024) # buffer size in bytes
+            reply = struct.unpack(data[0])
+            print "Reply from " + ": bytes=" + " time=" + "ms TTL=" + self.TTL
+            self.rcvd += 1
             logging.info(data[0])
             print data[0]
+
+            if self.rcvd == self.count:
+                Client.showSummary(self)
+
+    def showSummary(self):
+        print "Ping statistics for " + dst
+        print "  Packets: Sent = " + sent + ", Received =" + rcvd + ", Lost = " + lost + "(% loss)"
+        print "  Approximate round trip times in milli-seconds:"
+        print "  Minimum = " + "ms, Maximum =" + "ms, Average =" + "ms"
+
     
     def main(self, argv):
-        """Main function in CLIENT"""
+        """Main function in PING"""
 
         argv = sys.argv[1:]
+        logfile = ''
 
         try:
-            opts, args = getopt.getopt(argv, "hs:p:l:n:", ["serverIP=", "portno=", "logfile=", "myname="])
+            opts, args = getopt.getopt(argv, "hl:p:c:d:", ["logfile=", "payload=", "count=", "dst="])
         except getopt.GetoptError:
-            print('test.py -n <serverIP> -p <portno> -l <logfile> -n <myname>')
+            print('test.py -l <logfile> -p <payload> -c <count> -d <dst>')
             sys.exit(2)
         for opt, arg in opts:
             if opt == '-h':
-                print('test.py -n <serverIP> -p <portno> -l <logfile> -n <myname>')
+                print('test.py -l <logfile> -p <payload> -c <count> -d <dst>')
                 sys.exit()
-            elif opt in ("-s", "--serverIP"):
-                serverip = arg
-            elif opt in ("-p", "--portno"):
-                port = arg
             elif opt in ("-l", "--logfile"):
                 logfile = arg
-            elif opt in ("-n", "--myname"):
-                clientname = arg
+            elif opt in ("-p", "--payload"):
+                payload = arg
+            elif opt in ("-c", "--count"):
+                self.count = arg                
+            elif opt in ("-d", "--dst"):
+                dst = arg
 
         # Configure log file path, format, and clear file each time using write mode
-        logging.basicConfig(filename=logfile, level=logging.DEBUG, format='%(message)s', filemode ='w')
-        logging.info("connecting to the server " + serverip + " at port " + port)
-        
+        if logfile != '':
+            logging.basicConfig(filename=logfile, level=logging.DEBUG, format='%(message)s', filemode ='w')
+            logging.info("Payload: " + payload + ", Count: " + self.count + ", Destination: " + dst)
+
+        self.src = socket.gethostbyname(socket.gethostname())
+        print "Source IP: " + self.src
+        # packet = IP(src=src, dst=dst)/ICMP()
+        # srloop(packet, count=3)
+
         # For ease of use in instance function
-        port = int(port)        
-        self.serverip = serverip
+        port = int(80)        
         self.port = port
 
-        self.sock.sendto(("register " + clientname).encode(), (serverip, port))
-        logging.info("sending register message " + clientname)
+        # data = self.sock.recvfrom(1024) # buffer size in bytes
+        # welcomedata = data[0].split(' ')
+        # if welcomedata[0] == 'welcome':
+        #     client1 = welcomedata[1]
 
-        data = self.sock.recvfrom(1024) # buffer size in bytes
-        welcomedata = data[0].split(' ')
-        if welcomedata[0] == 'welcome':
-            client1 = welcomedata[1]
-
-        print "connected to server and registered"
+        print "Payload: " + payload + ", Count: " + self.count + ", Destination: " + dst
         logging.info("received welcome")
             
         # Thread setup
-        self.threadRecv = threading.Thread(target=Client.receive, args=(self,))
-        self.threadSend = threading.Thread(target=Client.send, args=(self,))
+        self.threadRecv = threading.Thread(target=Ping.receive, args=(self,))
+        self.threadSend = threading.Thread(target=Ping.send, args=(self,dst,payload))
         self.threadRecv.setDaemon(True)
         self.threadSend.setDaemon(True)
         self.threadRecv.start()
@@ -116,5 +124,5 @@ class Client():
         
         logging.info("terminating client..")
 
-client = Client()
-client.main(sys.argv[1:])
+ping = Ping()
+ping.main(sys.argv[1:])
